7f8f84b8ecef5ce90f004fc70c0da344344711e4
Rei <66063672+ReiChrysalis@users.noreply.github.com>
2021-11-10 20:50:43 +0300

    Update archtitus.sh

--- archtitus.sh
+++ archtitus.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
     bash 0-preinstall.sh
-    arch-chroot /mnt /root/ArchTitus/1-setup.sh
-    source /mnt/root/ArchTitus/install.conf
-    arch-chroot /mnt /usr/bin/runuser -u $username -- /home/$username/ArchTitus/2-user.sh
-    arch-chroot /mnt /root/ArchTitus/3-post-setup.sh
+    arch-chroot /mnt /root/rei/1-setup.sh
+    source /mnt/root/rei/install.conf
+    arch-chroot /mnt /usr/bin/runuser -u $username -- /home/$username/rei/2-user.sh
+    arch-chroot /mnt /root/rei/3-post-setup.sh
