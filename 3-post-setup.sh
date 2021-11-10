0047b70e4da46deff3cc1ef7ff728e10b2266609
Rei <66063672+ReiChrysalis@users.noreply.github.com>
2021-11-10 17:39:29 +0300

    Update 3-post-setup.sh

--- 3-post-setup.sh
+++ 3-post-setup.sh
@@ -21,17 +21,11 @@ grub-mkconfig -o /boot/grub/grub.cfg
 
 echo -e "\nEnabling Login Display Manager"
 systemctl enable sddm.service
-echo -e "\nSetup SDDM Theme"
-cat <<EOF > /etc/sddm.conf
-[Theme]
-Current=Nordic
-EOF
 
 # ------------------------------------------------------------------------
 
 echo -e "\nEnabling essential services"
 
-systemctl enable cups.service
 ntpd -qg
 systemctl enable ntpd.service
 systemctl disable dhcpcd.service
