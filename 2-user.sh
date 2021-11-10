832b6e0226dc1006ade1572ebb2d28128406255f
Rei <66063672+ReiChrysalis@users.noreply.github.com>
2021-11-10 20:53:40 +0300

    Update 2-user.sh

--- 2-user.sh
+++ 2-user.sh
@@ -53,9 +53,9 @@ for PKG in "${PKGS[@]}"; do
 done
 
 export PATH=$PATH:~/.local/bin
-cp -r $HOME/ArchTitus/dotfiles/* $HOME/.config/
+cp -r $HOME/rei/dotfiles/* $HOME/.config/
 pip install konsave
-konsave -i $HOME/ArchTitus/kde.knsv
+konsave -i $HOME/rei/kde.knsv
 sleep 1
 konsave -a kde
 
